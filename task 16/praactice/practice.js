c942d9590c4042dae2b32a32b59e8d74115f90df
muhammad <mkuysunov@gmail.com>
2019-12-05 17:32:52 +0500

    chanched ttask 16

--- task 16/praactice/practice.js
+++ task 16/praactice/practice.js
@@ -1,4 +1,3 @@
-// ========================================
 // my variant
 
 var Player = {
@@ -8,14 +7,29 @@ var Player = {
     played: false,
 
     next: function () {
-
+        if (this.played) {
+            this.currentItem++;
+        } else {
+            this.currentItem = 0;
+        }
     },
     prev: function () {
-
+        if (this.played && this.currentItem > 0) {
+            this.currentItem--;
+        } else {
+            this.currentItem = this.list.length - 1;
+        }
     },
-    getCurrent: function () {},
-    play: function () {},
-    addToList: function (listItem) {}
+    getCurrent: function () {
+        return this.list[this.currentItem];
+    },
+    play: function () {
+        this.played = true;
+        console.log(this.getCurrent());
+    },
+    addToList: function (listItem) {
+        this.list.push(listItem);
+    }
 };
 
 
@@ -23,17 +37,22 @@ var Player = {
 // VideoPlayer.list = [];
 
 var VideoPlayer = {
-        list: [],
-        uploadToYoutube: function (name) {
-
+    list: [],
+    uploadToYoutube: function (name) {
+        for (var i = 0; i < this.list.length; i++) {
+            if (this.list[i].name === name) {
+                console.log("upload to youtube", this.list[i]);
+                return
+            }
         }
+        console.log("can not find file " + name + "to upload to youtube");
     },
-    playFromYoutube: function (item) {},
-    // __proto__: Player, вместо прото исползывал  set..Of
+    playFromYoutube: function (item) {
+        console.log("Playing video from youtube:", item);
+    },
+    // __proto__: Player, вместо прото использывал  set..Of
 }
 Object.setPrototypeOf(VideoPlayer, Player);
-
-
 
 VideoPlayer.addToList({
     name: "Shashmaqom",
@@ -42,13 +61,18 @@ VideoPlayer.addToList({
     authot: "Tajik National"
 });
 
+VideoPlayer.addToList({
+    name: "Jazz",
+    year: 1966,
+    duration: "3,2m",
+    authot: "Foregn Music"
+});
+
 VideoPlayer.uploadToYoutube("Jazz");
 // console.log(VideoPlayer);
 
 // console.log(Player);
 // console.log("Player list:", Player.list);
-
-
 
 
 
